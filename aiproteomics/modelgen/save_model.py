d8a2321e44cb353c25ca70650c9171f3c0d97ead
DaniBodor <d.bodor@esciencecenter.nl>
2022-11-28 05:36:12 +0100

    save format from list instead of string

--- aiproteomics/modelgen/save_model.py
+++ aiproteomics/modelgen/save_model.py
@@ -2,24 +2,26 @@ import os
 import tf2onnx
 
 def save_model(
-        model,
-        input_format,
-        save_format, 
-        output_destination,
-        overwrite,
-        ):
+        model, 
+        input_format, 
+        save_format = 'onnx', 
+        output_location = './aiproteomics/modelgen/saved_models/', 
+        overwrite = True,
+    ):
+
+    if not isinstance(save_format, list):
+        save_format = [save_format]
+
+    if not os.path.exists(output_location):
+        os.makedirs(output_location)        
+
     # Save model
-    output_location = './aiproteomics/modelgen/saved_models/'
-    if save_format == None:
-        # do not save
-        pass
-    else:
-        if save_format == 'onnx' or save_format == 'both':
-            # save as onnx
-            output_path = output_location + model.name + ".onnx"
-            # using default opset and spec settings for now, might need to be hardcoded if it doesn't work for all cases in the future            
+    if input_format == 'keras':
+        if 'onnx' in save_format:
+            # using default opset and spec settings for now, 
+            # might need to be hardcoded if it doesn't work for all cases in the future            
             # for some idea on how to set this, see example on https://github.com/onnx/tensorflow-onnx/blob/main/tutorials/keras-resnet50.ipynb
+            output_path = output_location + model.name + ".onnx"
             tf2onnx.convert.from_keras(model, output_path=output_path)
-        if save_format == 'keras' or save_format == 'both':
-            # save as keras
+        if 'keras' in save_format:
             model.save(output_location + model.name)
