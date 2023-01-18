77514927fdb71925356bdf3b101c155542174729
Robin Richardson <raarichardson@gmail.com>
2023-01-18 17:26:52 +0100

    Add comments explaining every function in sanitize.py

--- aiproteomics/e2e/sanitize.py
+++ aiproteomics/e2e/sanitize.py
@@ -6,6 +6,12 @@ import functools
 from .constants import *
 
 def reshape_dims(array):
+    """
+    "Deflatten" the intensities array (from the size 174 prosit output layer)
+    to the form N_sequences x MAX_SEQUENCE - 1 x len(ION_TYPES) x nlosses x MAX_FRAG_CHARGE
+    For default output layer size 174, this corresponds to the shape:
+    N_sequences x 29 x 2 x 1 x 3
+    """
     n, dims = array.shape
     assert dims == 174
     nlosses = 1
@@ -16,31 +22,55 @@ def reshape_dims(array):
 
 
 def reshape_flat(array):
+    """
+    Reshape the given array to shape (N_sequences x flat_array_length)
+    where N_sequences is the length of the first axis,
+    and flat_array_length is the combined flattened length of all remaining axes.
+    For standard prosit defaults, this results in a shape of N_sequences x 174.
+    """
+
     s = array.shape
     flat_dim = [s[0], functools.reduce(lambda x, y: x * y, s[1:], 1)]
     return array.reshape(flat_dim)
 
 
 def normalize_base_peak(array):
-    # flat
+    """
+    Normalize by the highest intensity peak within each sequence
+    """
     maxima = array.max(axis=1)
     array = array / maxima[:, numpy.newaxis]
     return array
 
 
 def mask_outofrange(array, lengths, mask=-1.0):
-    # dim
+    """
+    Sets all values to mask for entries beyond the peptide sequence length.
+    The max length is 29 (for a peptide length of 30 amino acids) but many
+    sequences will be shorter, and so the unused remainder of the array is masked.
+    """
     for i in range(array.shape[0]):
         array[i, lengths[i] - 1 :, :, :, :] = mask
     return array
 
 
 def cap(array, nlosses=1, z=3):
+    """
+    Caps the neutral losses and frag charges to nlosses and z, respectively.
+
+    Caps the shape to (:, :, :, nlosses, z)
+    For example, if nlosses=1 and z=3, then
+    an array of shape (3, 29, 2, 3, 6) -> (3, 29, 2, 1, 3)
+    """
+
     return array[:, :, :, :nlosses, :z]
 
 
 def mask_outofcharge(array, charges, mask=-1.0):
-    # dim
+    """
+    For each quence, mask any entries corresponding to charges
+    greater than the threshold provided in 'charges'.
+    """
     for i in range(array.shape[0]):
         if charges[i] < 3:
             array[i, :, :, :, charges[i] :] = mask
