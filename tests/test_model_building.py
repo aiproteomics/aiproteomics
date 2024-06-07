75c5d24bac72ced01f83d5cdfee6d23be35311a2
raar1 <raarichardson@gmail.com>
2024-06-07 13:46:42 +0200

    Update tests for new frag model

--- tests/test_model_building.py
+++ tests/test_model_building.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from aiproteomics.rt.models import build_rt_transformer_model
-from aiproteomics.frag.models import build_model_transformer_encoder_prosit_decoder
-
+from aiproteomics.frag.models.early_fusion_transformer import build_model_early_fusion_transformer
 
 def test_build_rt_transformer_model():
     model_irt = build_rt_transformer_model(
@@ -18,14 +17,6 @@ def test_build_rt_transformer_model():
 
 
 def test_build_frag_transformer_model():
-    model_frag = build_model_transformer_encoder_prosit_decoder(
-        num_layers=6,  # number of layers
-        d_model=512,
-        num_heads=8,  # Number of attention heads
-        d_ff=2048,  # Hidden layer size in feed forward network inside transformer
-        dropout_rate=0.1,  #
-        vocab_size=22,  # number of aminoacids
-        max_len=30,  # maximal peptide length
-    )
+    model_frag = build_model_early_fusion_transformer()
     assert model_frag is not None
     assert isinstance(model_frag, tf.keras.Model)
