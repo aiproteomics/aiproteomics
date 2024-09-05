16686b4df93ca0ff1717c47967fd2c41963fd326
Djura Smits <djura.smits@gmail.com>
2024-09-05 15:37:06 +0200

    Cleaned up the notebook a bit

--- analyze_predictions.py
+++ analyze_predictions.py
@@ -13,6 +13,26 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# ## Introduction
+#
+# This notebook runs a couple of analyses on the original data and the predictions from the transformer model.
+# It can be run both locally and on snellius. When the [jupytext](https://jupytext.readthedocs.io/en/latest/) is installed,
+# this file `analyze_predictions.ipynb` syncs automatically with the script `analyze_predictions.py`.
+#
+# ### How to run on Snellius
+# The script `analyze_predictions.py` can be run on Snellius as follows.
+# ```bash
+# $ sbatch analyze_predictions.sh
+#
+# # sbatch: Single-node jobs run on a shared node by default. Add --exclusive if you want to use a node exclusively.
+# # sbatch: A full node consists of 72 CPU cores, 491520 MiB of memory and 4 GPUs and can be shared by up to 4 jobs.
+# # sbatch: By default shared jobs get 6826 MiB of memory per CPU core, unless explicitly overridden with --mem-per-cpu, --mem-per-gpu or --mem.
+# # sbatch: You will be charged for 1 GPUs, based on the number of CPUs, GPUs and the amount memory that you've requested.
+# # Submitted batch job 7740229
+#
+#
+
 # %%
 import numpy as np
 import h5py
@@ -29,17 +49,13 @@ import sys
 
 # %%
 PLOTS_DIR = Path("plots")
+DATA_DIR = Path("data")
 
 
 # %%
-# Turns out it was better to store the results in a hdf5 file
-# predictions = np.load("predictions.npy")
+# The shell script version of this analysis can take chunksize as an argument.
+# When running as a shell script it is also assumed that the analysis needs to be run on the full dataset.
 
-# with h5py.File("predictions.hdf5", "w") as f:
-#     dset = f.create_dataset("predictions", predictions.shape, predictions.dtype)
-#     dset[:, :] = predictions
-# %%
-# The following only makes sense as shell script
 try:
     print(sys.argv)
     chunksize = int(sys.argv[1])
@@ -50,8 +66,25 @@ except Exception:
     chunksize = 1000
     sample_size = 1000
 
+print(f"chunksize: {chunksize}, sample size: {sample_size}")
+
 # %%
 def load_dask_array_from_hdf5(filename, key, chunksize=1000, sample_size=-1):
+    """
+    Load data from hdf5 files and convert it to a dask array.
+    Dask uses lazy loading so chunksize ensures that only a limited amount of data
+    is loaded at any time.
+
+    Parameters:
+        filename: the hdf5 file
+        key: the key to the specific array
+        chunksize: chunksize of the array. Default is 1000
+        sample_size: the amount of samples to load. Default is to load the full dataset
+
+    Returns:
+        array: a dask array
+    """
+    
     f = h5py.File(filename)
     a = da.from_array(f[key], chunks=chunksize)
     a = a[:sample_size]
@@ -59,30 +92,27 @@ def load_dask_array_from_hdf5(filename, key, chunksize=1000, sample_size=-1):
 
 
 # %%
-predictions = load_dask_array_from_hdf5("predictions.hdf5", "predictions", chunksize, sample_size)
-labels = load_dask_array_from_hdf5("traintest_hcd.hdf5", "intensities_raw", chunksize, sample_size)
-collision_energy = load_dask_array_from_hdf5("traintest_hcd.hdf5", "collision_energy", chunksize, sample_size)
-precursor_charge = load_dask_array_from_hdf5("traintest_hcd.hdf5", "precursor_charge_onehot", chunksize, sample_size)
-sequences = load_dask_array_from_hdf5("traintest_hcd.hdf5", "sequence_integer", chunksize, sample_size)
+predictions = load_dask_array_from_hdf5(DATA_DIR/"predictions.hdf5", "predictions", chunksize, sample_size)
+labels = load_dask_array_from_hdf5(DATA_DIR/"traintest_hcd.hdf5", "intensities_raw", chunksize, sample_size)
+collision_energy = load_dask_array_from_hdf5(DATA_DIR/"traintest_hcd.hdf5", "collision_energy", chunksize, sample_size)
+precursor_charge = load_dask_array_from_hdf5(DATA_DIR/"traintest_hcd.hdf5", "precursor_charge_onehot", chunksize, sample_size)
+sequences = load_dask_array_from_hdf5(DATA_DIR/"traintest_hcd.hdf5", "sequence_integer", chunksize, sample_size)
 
 # %%
-full_sequences = sequences[:,-1] > 0
-
-full_sequences.shape
-
-# %%
+# Calculating sequence length by finding the first occurrence of 0
 sequence_lengths= da.argmin(sequences, axis=1)
 
-# %%
+# Full sequences have no 0s so we have to do something different.
+full_sequences = sequences[:,-1] > 0
+full_sequences.shape
+
+# Fix the lengths of the full sequences
 sequence_lengths[full_sequences] = 29
 
 # %%
-f = h5py.File("traintest_hcd.hdf5")
+f = h5py.File(DATA_DIR/"traintest_hcd.hdf5")
 
 f.keys()
-
-# %%
-["collision_energy_aligned_normed", "precursor_charge_onehot", "sequence_integer"]
 
 # %%
 labels.shape
@@ -155,28 +185,17 @@ concatenated
 concatenated.head()
 
 # %%
-# Let's take a sample to quickly try out analyses
-sample_size= 100000
-sample = concatenated.head(sample_size)
 
-# %%
-collision_energy[:3].compute()
+
+
 
 # %%
 corr_columns = ["sequence_length", "collision_energy", "distance"]
 
 with ProgressBar():
-    sample = concatenated[corr_columns].head(100000)
-    correlations = sample.corr()
-    
-correlations
+    correlations = concatenated[corr_columns].corr().compute()
 
-
-# %%
-with ProgressBar():
-    full_set_correlations = concatenated[corr_columns].corr().compute()
-
-sns.heatmap(full_set_correlations)
+sns.heatmap(correlations)
 
 plt.savefig(PLOTS_DIR/"correlations")
 
@@ -184,11 +203,6 @@ plt.savefig(PLOTS_DIR/"correlations")
 # It seems that sequence length and distance have some correlation. I can imagine that longer sequences are more easy to predict because there is more info there? Or maybe there are unique sequences that are memorized. I wonder what the distribution is of sequence length.
 
 # %%
-sample.sequence_length.hist()
-
-# %%
-# On the full set
-
 freq, bins = da.histogram(concatenated["sequence_length"], bins=range(30))
 
 with ProgressBar():
@@ -254,5 +268,3 @@ best_performing
 
 # %%
 best_performing.iloc[0]
-
-# %%
