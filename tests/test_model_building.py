8b3f2a555251db46e2a3d755c6d30ce365f3eef7
raar1 <raarichardson@gmail.com>
2025-02-12 10:59:36 +0100

    Add basic documentation for model building tests

--- tests/test_model_building.py
+++ tests/test_model_building.py
@@ -6,6 +6,10 @@ from aiproteomics.models.prosit1 import generate_prosit1_model
 
 
 def test_build_dummy_msms_model():
+    """
+    Tests generating a (dummy) MSMS model using a `ModelParamsMSMS` class
+    """
+
     params = ModelParamsMSMS(seq_len=50, ions=['y','b'], max_charge=2, neutral_losses=['', 'H3PO4'])
     nn_model, creation_meta = generate_dummy_msms_model(
         params=params,
@@ -18,7 +22,11 @@ def test_build_dummy_msms_model():
     assert isinstance(creation_meta, dict)
 
 
-def test_build_dummy_msms_model():
+def test_build_dummy_iRT_model():
+    """
+    Tests generating a (dummy) MSMS model using a `ModelParamsRT` class
+    """
+
     params = ModelParamsRT(seq_len=50, iRT_rescaling_mean=101.11514, iRT_rescaling_var=46.5882)
     nn_model, creation_meta = generate_dummy_iRT_model(
         params=params,
@@ -31,7 +39,11 @@ def test_build_dummy_msms_model():
     assert isinstance(creation_meta, dict)
 
 
-def test_build_dummy_msms_model():
+def test_build_dummy_ccs_model():
+    """
+    Tests generating a (dummy) CCS model using a `ModelParamsMSMS` class
+    """
+
     params = ModelParamsCCS(seq_len=50)
     nn_model, creation_meta = generate_dummy_ccs_model(
         params=params,
@@ -45,6 +57,10 @@ def test_build_dummy_msms_model():
 
 
 def test_build_prosit1_model():
+    """
+    Tests generating an implementation of the original 2019 Prosit model.
+    """
+
     nn_model, creation_meta = generate_prosit1_model()
     assert isinstance(nn_model, tf.keras.Model)
     assert isinstance(creation_meta, dict)
